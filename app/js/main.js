649566ddf4bbfb3ff6835ddff9039b9c13969634
paparatce <kapustakvaha@gmail.com>
2020-04-23 11:10:43 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,34 @@ $(function () {
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4,
+        responsive: [
+            {
+              breakpoint: 1900,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+                  {
+                    breakpoint: 1441,
+                    settings: {
+                      slidesToShow: 2,
+                      slidesToScroll: 2,
+                      infinite: true,
+                      dots: true
+                    }
+                  }, {
+                    breakpoint: 801,
+                    settings: {
+                      slidesToShow: 1,
+                      slidesToScroll: 1,
+                      infinite: true,
+                      dots: true
+                    }
+                  },
+          ]
     });
     $(".js-range-slider").ionRangeSlider({
         type: "double",
@@ -51,8 +79,5 @@ $('input[type="file"],select').styler();
         return false;
     });
 
-    $(".login__wrapper").on("click", function (){
-        $(".modal").show(500);
-      });
     var mixer = mixitup('.products__inner-box')
 });
